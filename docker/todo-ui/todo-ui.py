1f2d92de7a603630fca028870296bafd5e947727
Hari <hariprasad.karthigasu@lunatech.nl>
2020-03-12 21:01:21 +0100

    DEV-100320 Update manifests and add todo-ui

--- docker/todo-ui/todo-ui.py
+++ docker/todo-ui/todo-ui.py
@@ -11,42 +11,40 @@ from json2html import *
 
 MONGO_HOST          = os.getenv('MONGO_HOST')
 MONGO_PORT          = int(os.getenv('MONGO_PORT'))
-MONGO_DATABASE      = os.getenv('MONGO_DB')
+MONGO_DATABASE      = os.getenv('MONGO_DATABASE')
 MONGO_COLLECTION    = os.getenv('MONGO_COLLECTION')
-MONGO_USERNAME      = os.getenv('MONGO_USER')
+MONGO_USERNAME      = os.getenv('MONGO_USERNAME')
 MONGO_PASSWORD      = os.getenv('MONGO_PASSWORD')
+TODO_SERVICE_IP     = os.getenv('TODO_SERVICE_IP')
+TODO_SERVICE_PORT   = int(os.getenv('TODO_SERVICE_PORT'))
 
 
 def connectDB():
-    client = MongoClient("mongodb://%s:%s@%s:%d/" % (MONGO_USER,MONGO_PASSWORD,MONGO_HOST,MONGO_PORT))
+    client = MongoClient("mongodb://%s:%s@%s:%d/" % (MONGO_USERNAME,MONGO_PASSWORD,MONGO_HOST,MONGO_PORT))
     return client
 
-def getPassengerDict(request_body):
-    passenger = collections.OrderedDict()
+def getTodoDict(request_body):
+    todo = collections.OrderedDict()
     
-    passenger["_id"]                        = ObjectId(request_body['_id'])
-    passenger["survived"]                   = bool(request_body['survived'])
-    passenger["pclass"]                     = int(request_body['pclass'])
-    passenger["name"]                       = request_body['name']
-    passenger["sex"]                        = request_body['sex']
-    passenger["age"]                        = int(request_body['age'])
-    passenger["siblings_spouses_aboard"]    = int(request_body['siblings_spouses_aboard'])
-    passenger["parents_children_aboard"]    = int(request_body['parents_children_aboard'])
-    passenger["fare"]                       = float(request_body['fare'])
+    todo["_id"]        = ObjectId(request_body['_id'])
+    todo["task"]       = request_body['task']
+    todo["duedate"]    = request_body['duedate']
+    todo["labels"]     = request_body['labels']
+    todo["comments"]   = request_body['comments']
 
-    return passenger   
+    return todo   
 
 @route('/ui', method='GET')
-def viewPassengers():
+def viewTodos():
     client = connectDB()
-    db = client[MONGO_DB]
+    db = client[MONGO_DATABASE]
     results = list(db[MONGO_COLLECTION].find({}))
     ordered_results = []
     for index in range(0, len(results)):
-        ordered_results.append(getPassengerDict(results[index]))   
+        ordered_results.append(getTodoDict(results[index]))   
     response = json.loads(json_util.dumps(ordered_results))
     client.close() 
     return template("{{!data}}", data=json2html.convert(json = response))
 
 if __name__ == '__main__':
-    run(host='0.0.0.0', port=9038, debug=True)  
+    run(host=TODO_SERVICE_IP, port=TODO_SERVICE_PORT, debug=True)  
